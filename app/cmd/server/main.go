5b6dbf8a8fc738781ca850338635e113c0f76269
Toshiyuki Sato <sato-toshiyuki@dmm.com>
2023-01-27 21:00:10 +0900

    Implement entrypoint

--- app/cmd/server/main.go
+++ app/cmd/server/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"os"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+
+	"github.com/toshiykst/go-layerd-architecture/app/handler"
+	"github.com/toshiykst/go-layerd-architecture/app/infrastructure/database"
+	"github.com/toshiykst/go-layerd-architecture/app/usecase"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	ctx := context.Background()
+
+	// TODO: Use env config
+	db := database.NewDBRepository(ctx, database.Config{
+		User:     os.Getenv("MYSQL_USER"),
+		Password: os.Getenv("MYSQL_PASSWORD"),
+		Host:     os.Getenv("MYSQL_HOST"),
+		DBName:   os.Getenv("MYSQL_DATABASE"),
+		Debug:    os.Getenv("MYSQL_DEBUG") == "true",
+	})
+
+	uuc := usecase.NewUserUsecase(db)
+	uh := handler.NewUserHandler(uuc)
+
+	e.GET("/users/:id", uh.GetUser)
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
