d2825dbadd17b15ceaca058d22726768d185c193
Toshiyuki Sato <sato-toshiyuki@dmm.com>
2023-01-27 20:47:31 +0900

    Fix init.sql

--- local/mysql/init.sql
+++ local/mysql/init.sql
@@ -14,10 +14,10 @@ CREATE TABLE IF NOT EXISTS `groups` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `group_users` (
-    `group_id`    VARCHAR(255) PRIMARY KEY NOT NULL,
-    `user_id`     VARCHAR(255) PRIMARY KEY NOT NULL,
+    `group_id`    VARCHAR(255) NOT NULL,
+    `user_id`     VARCHAR(255) NOT NULL,
     `created_at`  TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-
+    PRIMARY KEY(`group_id`,`user_id`),
     CONSTRAINT `fk_group_users_group_id` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`),
     CONSTRAINT `fk_group_users_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
