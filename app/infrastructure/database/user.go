9440ab6d2e5f0b551625e7863769b32f91661e43
Toshiyuki Sato <sato-toshiyuki@dmm.com>
2023-01-28 15:16:46 +0900

    Implement create of db user repository

--- app/infrastructure/database/user.go
+++ app/infrastructure/database/user.go
@@ -4,6 +4,7 @@ import (
 	"gorm.io/gorm"
 
 	"github.com/toshiykst/go-layerd-architecture/app/domain/model"
+	"github.com/toshiykst/go-layerd-architecture/app/infrastructure/database/datamodel"
 )
 
 type dbUserRepository struct {
@@ -23,7 +24,13 @@ func (db *dbUserRepository) List() ([]*model.User, error) {
 }
 
 func (db *dbUserRepository) Create(u *model.User) (*model.User, error) {
-	return nil, nil
+	dmu := datamodel.NewUser(u.ID, u.Name, u.Name)
+
+	if err := db.conn.Create(dmu).Error; err != nil {
+		return nil, err
+	}
+
+	return datamodel.ToUserModel(dmu), nil
 }
 
 func (db *dbUserRepository) Update(u *model.User) error {
